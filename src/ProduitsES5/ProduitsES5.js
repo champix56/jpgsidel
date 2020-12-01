b4f4e4d68d2e1fe3452778acc9d53fbedcd4c0a5
alex <desorbaix@free.fr>
2020-12-01 20:58:12 +0100

    updt

--- src/ProduitsES5/ProduitsES5.js
+++ src/ProduitsES5/ProduitsES5.js
@@ -6,20 +6,22 @@ function Produits() {
     var _liste = [];
     this.get = function (index) { return _liste[index]; }
     this.cloneAt = function (index) { return Object.assign({}, _liste[index]); }
-    //exposition de fonctions tierce d'un contenu privé
+
+    //attention a push qui poussera sur this et non sur la liste pas d'exposition direct -->fermeture
     this.add = function (value) { return _liste.push(value) };
-    //this.add =  _liste.push;
+
+    //exposition de fonctions tierce d'un contenu privé
+
     this.pop = _liste.pop;
     this.find = _liste.find;
     this.findAt = _liste.findIndex;
-
+    this.clog = function (message) { return console.log(message ? message : '', _liste) }
+    this.clog('end init')
 }
 
 var p2 = new Produits();
 p2.add(new Produit({ nom: 'anc' }));
 p2.clog('outclass')
-// p2.add(new Produit({ nom: 'an2' }));
-// p2.add(new Produit({ nom: 'an3' }));
-// p2.add(new Produit({ nom: 'an4' }));
+
 console.log(p2);
 console.log(p2[0]);
