de3091d368d4b2aed9d05ea010a329f4208619fb
alex <desorbaix@free.fr>
2020-12-02 14:42:27 +0100

    promise avec synchro

--- src/produitsES6/ProduitsES6.js
+++ src/produitsES6/ProduitsES6.js
@@ -1,27 +1,58 @@
 
-class Produits extends Array{
-    constructor()
-    {
+class Produits extends Array {
+    #_template=null;
+    constructor() {
         super();
     }
-    loadFromRest(){
-
+    loadTemplate(url = 'vues/produits-produit.html') {
+        return fetch(url).then((resp) => { return resp.text() })
+            .then((texthtml) => {
+                const parser = new DOMParser();
+                const template = parser.parseFromString(texthtml, 'text/html').body.childNodes;
+                console.log(template.innerHTML);
+                return template;
+            });
     }
-    findByEAN(EAN){
-       let obj= this.find(
-            (element)=>{
-                return element.EAN===EAN;
+    loadFromRest() {
+        return fetch('http://localhost:5629/produits')
+            .then(function (fluxResponse) { return fluxResponse.json() })
+            .then((objetJS) => {
+
+                console.log('Voici la valeur recu', objetJS);
+                objetJS.map((element, index) => {
+                    this.push(element);
+                });
+                console.log(this);
+                return objetJS;
+            })
+    }
+    /**
+     * lancement synchronisé des chargements par promises
+     */
+    syncLoad() {
+        Promise.all([this.loadFromRest(), this.loadTemplate()])
+            .then(responses => {
+                console.log(responses)
+            });
+    }
+    findByEAN(EAN) {
+        let obj = this.find(
+            (element) => {
+                return element.EAN === EAN;
             }
         );
         return obj;
     }
 
 }
-const produits=new Produits();
-produits.push(new Produit({nom:'Produit1', EAN:'0000'}));
-produits.push(new Produit({nom:'Produit2', EAN:'0001'}));
-produits.push(new Produit({nom:'Produit3', EAN:'0002'}));
-produits.push(new Produit({nom:'Produit4', EAN:'0003'}));
+const produits = new Produits();
+// produits.loadFromRest();
+// produits.loadTemplate();
+produits.syncLoad();
+produits.push(new Produit({ nom: 'Produit1', EAN: '0000' }));
+produits.push(new Produit({ nom: 'Produit2', EAN: '0001' }));
+produits.push(new Produit({ nom: 'Produit3', EAN: '0002' }));
+produits.push(new Produit({ nom: 'Produit4', EAN: '0003' }));
 
 
 console.log(produits);
