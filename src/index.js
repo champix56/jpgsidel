f76cb3c2d350494f3801b2bb25e20dd54f354a93
alex <desorbaix@free.fr>
2020-12-03 16:18:18 +0100

    j3 avant test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import Produits from './produitsES6/ProduitsES6.js';
 import {formulaireProduit} from './ProduitForm/ProduitForm.js';
 
+(
+    function(){
+        document.querySelector('#newProduct').addEventListener('click',(event) => {
+            formulaireProduit.produit=null;
+        })
+    }
+)();
+
