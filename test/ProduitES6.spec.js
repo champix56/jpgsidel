bae0764d986a611f1e34f4c6475110e6a147ef6a
alex <desorbaix@free.fr>
2020-12-03 16:30:12 +0100

    ecriture test

--- test/ProduitES6.spec.js
+++ test/ProduitES6.spec.js
@@ -1,7 +1,16 @@
 var Produit = require('../src/produitsES6/ProduitsES6.js')
-
+var assert  = require('assert');
 describe('Test de la function produit', () => {
     describe('#test constructeur', () => {
+        it('should instanciate without good EAN', () => {
+            var produit = new Produit({nom:'',EAN:''});
+            var EAN=produit.EAN;
+            assert.strictEqual(EAN,undefined);
+            
+            var produit = new Produit({nom:'',EAN:'1234567891234'});
+            assert.strictEqual(EAN,'1234567891234');
+            assert.strictEqual(produit.EAN.length,13);
+        });
         
     });
     describe('#test affichage DOM', () => {
