08fca821ed72d3a74fd6a29d153329e1e8c87934
alex <desorbaix@free.fr>
2020-12-03 16:18:28 +0100

    debut des tests

---
+++ test/ProduitES6.spec.js
@@ -0,0 +1,12 @@
+var Produit = require('../src/produitsES6/ProduitsES6.js')
+
+describe('Test de la function produit', () => {
+    describe('#test constructeur', () => {
+        
+    });
+    describe('#test affichage DOM', () => {
+        
+    });
+    
+    
+});
